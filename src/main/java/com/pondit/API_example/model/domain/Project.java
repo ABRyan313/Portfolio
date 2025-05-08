88877c46daf164b6b85ee6679f7370784dfa945c
Md. Abir Hossain <dreamshadow.ab@gmail.com>
2025-05-08 22:51:50 +0600

    Get and post apply using API client

---
+++ src/main/java/com/pondit/API_example/model/domain/Project.java
@@ -0,0 +1,20 @@
+package com.pondit.API_example.model.domain;
+
+public class Project {
+
+    private String name;
+    private String description;
+
+    public Project(String name, String description ) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
---
+++ src/main/java/com/pondit/API_example/model/dto/ProjectDTO.java
@@ -0,0 +1,23 @@
+package com.pondit.API_example.model.dto;
+
+public class ProjectDTO {
+
+    private String name;
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
