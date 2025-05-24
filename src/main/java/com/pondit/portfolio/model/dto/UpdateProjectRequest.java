27c2c558bc43e7d860773c8369aef2471b668202
Md. Abir Hossain <dreamshadow.ab@gmail.com>
2025-05-24 20:46:37 +0600

    Add update project request class in DTO

---
+++ src/main/java/com/pondit/portfolio/model/dto/UpdateProjectRequest.java
@@ -0,0 +1,23 @@
+package com.pondit.portfolio.model.dto;
+
+public class UpdateProjectRequest {
+
+    private String name;
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
