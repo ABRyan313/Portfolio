e3c557b673024ad6330ece5553b5d47f774abcc7
Md. Abir Hossain <dreamshadow.ab@gmail.com>
2025-05-29 23:54:12 +0600

    Add ResumeConfig, Update RootController

---
+++ src/main/java/com/pondit/portfolio/config/ResumeConfig.java
@@ -0,0 +1,166 @@
+package com.pondit.portfolio.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+@ConfigurationProperties(prefix = "resume")
+public class ResumeConfig {
+    private PersonalInfo personalInfo;
+    private List<Education> education;
+    private List<Experience> experience;
+    private List<String> skills;
+
+    public PersonalInfo getPersonalInfo() {
+        return personalInfo;
+    }
+
+    public void setPersonalInfo(PersonalInfo personalInfo) {
+        this.personalInfo = personalInfo;
+    }
+
+    public List<Education> getEducation() {
+        return education;
+    }
+
+    public void setEducation(List<Education> education) {
+        this.education = education;
+    }
+
+    public List<Experience> getExperience() {
+        return experience;
+    }
+
+    public void setExperience(List<Experience> experience) {
+        this.experience = experience;
+    }
+
+    public List<String> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(List<String> skills) {
+        this.skills = skills;
+    }
+
+    public static class PersonalInfo{
+        private String name;
+        private String email;
+        private String LinkedInUrl;
+        private String GitHubUrl;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getLinkedInUrl() {
+            return LinkedInUrl;
+        }
+
+        public void setLinkedInUrl(String linkedInUrl) {
+            LinkedInUrl = linkedInUrl;
+        }
+
+        public String getGitHubUrl() {
+            return GitHubUrl;
+        }
+
+        public void setGitHubUrl(String gitHubUrl) {
+            GitHubUrl = gitHubUrl;
+        }
+    }
+
+    public static class Education{
+        private String degree;
+        private String institution;
+        private String years;
+        private String description;
+
+        public String getDegree() {
+            return degree;
+        }
+
+        public void setDegree(String degree) {
+            this.degree = degree;
+        }
+
+        public String getInstitution() {
+            return institution;
+        }
+
+        public void setInstitution(String institution) {
+            this.institution = institution;
+        }
+
+        public String getYears() {
+            return years;
+        }
+
+        public void setYears(String years) {
+            this.years = years;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+    }
+
+    public static class Experience{
+        private String title;
+        private String company;
+        private String years;
+        private String description;
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getCompany() {
+            return company;
+        }
+
+        public void setCompany(String company) {
+            this.company = company;
+        }
+
+        public String getYears() {
+            return years;
+        }
+
+        public void setYears(String years) {
+            this.years = years;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+    }
+
+
+}
