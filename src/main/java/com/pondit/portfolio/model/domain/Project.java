ea52b9f66e09ee88aedc1fe2a2e9a78b02a80a16
Md. Abir Hossain <dreamshadow.ab@gmail.com>
2025-06-12 21:17:37 +0600

    Refactor: Move @Getter annotation to class level.

--- src/main/java/com/pondit/portfolio/model/domain/Project.java
+++ src/main/java/com/pondit/portfolio/model/domain/Project.java
@@ -1,26 +1,19 @@
 package com.pondit.portfolio.model.domain;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Project {
 
+
     private Long id;
-    private String name;
+
+
+    public String name;
+
+
     private String description;
-
-    public Project(Long id, String name, String description ) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
 }
--- src/main/java/com/pondit/portfolio/persistance/entity/ProjectEntity.java
+++ src/main/java/com/pondit/portfolio/persistance/entity/ProjectEntity.java
@@ -4,35 +4,21 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
 @Table(name = "project")
 @Entity
 public class ProjectEntity {
     @Id
     @GeneratedValue
     private Long id;
+
+    @Setter
     private String name;
+
+    @Setter
     private String description;
 
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
