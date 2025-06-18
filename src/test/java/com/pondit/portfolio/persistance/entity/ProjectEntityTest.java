4c165f52ffa93fe66436a5bbcc60fbf7b2e37c12
Md. Abir Hossain <dreamshadow.ab@gmail.com>
2025-06-18 22:30:21 +0600

    Implement blog and resume pages with new controllers and views

---
+++ src/test/java/com/pondit/portfolio/persistance/entity/ProjectEntityTest.java
@@ -0,0 +1,28 @@
+package com.pondit.portfolio.persistance.entity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProjectEntityTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void setDescription() {
+    }
+}
