1522c935b723715832170888b108b746d05847cd
Md. Abir Hossain <dreamshadow.ab@gmail.com>
2025-06-22 21:23:59 +0600

    fix BeanUtils unable to map (Entity to Domain)

--- src/main/java/com/pondit/portfolio/model/domain/Project.java
+++ src/main/java/com/pondit/portfolio/model/domain/Project.java
@@ -2,18 +2,15 @@ package com.pondit.portfolio.model.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Project {
 
-
     private Long id;
-
-
     public String name;
-
-
     private String description;
 }
